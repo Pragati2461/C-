12cc2f79b491f509ddf2152ed46aa033f8c813f5
Pragati2461 <Pragatibatra2310@gmail.com>
2022-05-24 23:31:28 +0530

    operators

---
+++ question3.cpp
@@ -0,0 +1,18 @@
+// using logical operators
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the value of a here:"<<endl;
+    cin>>a;
+    cout<<"Enter the value of b here:"<<endl;
+    cin>>b;
+    int sum;
+    cout<<"Sum of a and b is:"<<a+b<<endl;
+    cout<<"Subtraction of a and b is:"<<a-b<<endl;
+    cout<<"Multiplication of a and b is:"<<a*b<<endl;
+    cout<<"Division of a and b is:"<<a/b<<endl;
+    return 0;
+
+
+}
